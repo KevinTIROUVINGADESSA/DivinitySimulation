05a42e50416a4f1c7d70ba5d61a6d38d51695187
Alexc75 <carlotalexandre75@gmail.com>
2019-04-03 20:11:16 +0200

    Resolution bogs git

--- population.js
+++ population.js
@@ -1,15 +1,52 @@
+const {Guerrier} = require('./guerrier');
+const {Marchand} = require('./marchand');
+
 class Population {
+
+    guerriers;
+    marchands;
+    peons;
 
     constructor(nbPeon, nbMarchand, nbGuerrier) {
         this.nbPeon = Math.floor(nbPeon);
         this.nbMarchand = Math.floor(nbMarchand);
         this.nbGuerrier = Math.floor(nbGuerrier);
+        this.init();
     }
+
+    init() {
+        for (var i = 0; i < this.nbGuerrier; i++)
+        {
+            this.guerriers[i] = new Guerrier(Math.random()*100, Math.random()*100,
+                Math.random()*100, Math.random()*2)
+        }
+        for (var i = 0; i < this.nbMarchand; i++)
+        {
+            this.marchands[i] = new Marchand(Math.random()*100, Math.random()*100,
+                Math.random()*100, Math.random()*100);
+        }
+        /*for (var i = 0; i < this.nbPeon; i++)
+        {
+            this.peons[i] = new Peons(Math.random()*100, Math.random()*100,
+                Math.random()*100, Math.random()*100);
+        }*/
+
+    }
+
     showPop() {
         console.log("La population est constituee de :");
         console.log(`Nombre de marchands : ${this.nbMarchand}`);
+        for (var i = 0; i < this.nbMarchand; i++)
+        {
+            console.log(`${this.marchands[i].Decrire()}`);
+        }
         console.log(`Nombre de guerriers : ${this.nbGuerrier}`);
+        for (var i = 0; i < this.nbGuerrier; i++)
+        {
+            console.log(`${this.guerriers[i].Decrire()}`);
+        }
         console.log(`Nombre de peons : ${this.nbPeon}`);
+
     }
 
     reviveGue() {
