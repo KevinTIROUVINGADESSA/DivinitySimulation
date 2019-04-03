fa72fa241899d753fbe82632bb9626506ec10419
Kevin25T <kevin.tirouvingadessa@yahoo.fr>
2019-04-03 20:00:24 +0200

    Ajout de marchand.js et guerrier.js

--- marchand.js
+++ marchand.js
@@ -4,52 +4,51 @@ class Marchand {
 
     constructor(stack_corn,stack_gold,nbrecorn,nbregold)
     {
-        this.stack_corn = stack_corn;
-        this.stack_gold = stack_gold;
-        this.gold = nbregold;
-        this.corn = nbrecorn;
-
+        this.max_corn = stack_corn;
+        this.max_gold = stack_gold;
+        this.actual_gold = nbregold;
+        this.actual_corn = nbrecorn;
     }
 
 
     Decrire()
     {
-        console.log(`Votre marchand a ${this.gold} golds et ${this.corn} corns, il peut porter ${this.stack_corn} corns ${this.stack_gold} golds`);
+        console.log(`Votre marchand a ${this.actual_gold} golds et ${this.actual_corn} corns, il peut porter ${this.max_corn} corns ${this.max_gold} golds`);
     }
 
     Echange_gold_to_corn(m,nbregold)
     {
         let corn_for_exchange = 2*nbregold;
-        if(this.corn + corn_for_exchange > this.stack_corn)
+        if(this.actual_corn + corn_for_exchange > this.max_corn)
         {
-            console.log("Votre marchand ne peut pas porter autant de corn")
+            console.log("Votre marchand ne peut pas porter autant de corn");
         }
-        else if (this.gold-nbregold<0)
+        else if (this.actual_gold-nbregold<0)
         {
             console.log("TA PA DARGENT CLOCHARD");
         }
         else
         {
-            this.gold = this.gold - nbregold;
-            this.corn = this.corn + corn_for_exchange;
+            this.actual_gold = this.actual_gold - nbregold;
+            this.actual_corn = this.actual_corn + corn_for_exchange;
         }
     }
 
     Echange_corn_to_gold(m,nbrecorn)
     {
         let gold_for_exchange = (1/2)*nbrecorn;
-        if(this.gold + gold_for_exchange > this.stack_gold)
+        if(this.actual_gold + gold_for_exchange > this.max_gold)
         {
             console.log("Votre marchand ne peut pas porter autant de gold")
         }
-        else if (this.corn-nbrecorn<0)
+        else if (this.actual_corn-nbrecorn<0)
         {
             console.log("PAS ASSEZ DE CORN");
         }
         else
         {
-            this.corn = this.corn - nbrecorn;
-            this.gold = this.gold + 2 * gold_for_exchange;
+            this.actual_corn = this.actual_corn - nbrecorn;
+            this.actual_gold = this.actual_gold + 2 * gold_for_exchange;
         }
     }
 
