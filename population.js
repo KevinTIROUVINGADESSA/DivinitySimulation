5ddc890fd60802f3983df58bc65d04e3d7934274
Alexc75 <carlotalexandre75@gmail.com>
2019-04-07 23:46:47 +0300

    I love rock and roll

--- population.js
+++ population.js
@@ -2,76 +2,80 @@ const {Guerrier} = require('./guerrier');
 const {Marchand} = require('./marchand');
 
 class Population {
+  constructor(nbMarchand, nbGuerrier) {
+    this.nbMarchand_ = nbMarchand;
+    this.nbGuerrier_ = nbGuerrier;
+    this.guerriers_ = [];
+    this.marchands_ = [];
+    this.init();
+  }
 
-    constructor(nbMarchand, nbGuerrier) {
-        this.nbMarchand_ = nbMarchand;
-        this.nbGuerrier_ = nbGuerrier;
-        this.guerriers_ = [];
-        this.marchands_ = [];
-        this.init();
+  init() {
+    for (let i = 0; i < this.nbGuerrier_; i++) {
+      this.guerriers_[i] = new Guerrier(
+        Math.floor(Math.random() * 50),
+        Math.floor(Math.random() * 50),
+        Math.floor(Math.random() * 50),
+        Math.random()
+      );
+    }
+    for (let i = 0; i < this.nbMarchand_; i++) {
+      this.marchands_[i] = new Marchand(
+        Math.floor(1000 / this.nbMarchand_),
+        Math.floor(1000 / this.nbMarchand_)
+      );
+    }
+  }
+
+  showPop() {
+    let now;
+    console.log('La population est constituee de :');
+    console.log(`Nombre de marchands : ${this.nbMarchand_}`);
+    for (let i = 0; i < this.nbMarchand_; i++) {
+      this.marchands_[i].Decrire();
+      now = new Date().getTime();
+      while (new Date().getTime() < now + 500) {}
     }
 
-    init() {
-        for (let i = 0; i < this.nbGuerrier_; i++)
-        {
-            this.guerriers_[i] = new Guerrier(Math.floor(Math.random()*50), Math.floor(Math.random()*50),
-                Math.floor(Math.random()*50), Math.random());
-        }
-        for (let i = 0; i < this.nbMarchand_; i++)
-        {
-            this.marchands_[i] = new Marchand(Math.floor(1000/this.nbMarchand_), Math.floor(1000/this.nbMarchand_)
-                );
-        }
+    console.log(`Nombre de guerriers : ${this.nbGuerrier_}`);
+    for (let i = 0; i < this.nbGuerrier_; i++) {
+      this.guerriers_[i].Decrire();
+      now = new Date().getTime();
+      while (new Date().getTime() < now + 500) {}
     }
+  }
 
-    showPop() {
-        let now;
-        console.log("La population est constituee de :");
-        console.log(`Nombre de marchands : ${this.nbMarchand_}`);
-        for (let i = 0; i < this.nbMarchand_; i++) {
-            this.marchands_[i].Decrire();
-            now = new Date().getTime();
-            while(new Date().getTime() < now + 500) { }
-        }
-        console.log(`Nombre de guerriers : ${this.nbGuerrier_}`);
-        for (let i = 0; i < this.nbGuerrier_; i++) {
-            this.guerriers_[i].Decrire();
-            now = new Date().getTime();
-            while(new Date().getTime() < now + 500) { }
-        }
-    }
+  get nbMarchand() {
+    return this.nbMarchand_;
+  }
 
-    get nbMarchand() {
-        return this.nbMarchand_;
-    }
+  set nbMarchand(nm) {
+    this.nbMarchand_ = nm;
+  }
 
-    set nbMarchand(nm) {
-        this.nbMarchand_ = nm;
-    }
+  get nbGuerrier() {
+    return this.nbGuerrier_;
+  }
 
-    get nbGuerrier() {
-        return this.nbGuerrier_;
-    }
+  set nbGuerrier(ng) {
+    this.nbGuerrier_ = ng;
+  }
 
-    set nbGuerrier(ng) {
-        this.nbGuerrier_ = ng;
-    }
+  get guerriers() {
+    return this.guerriers_;
+  }
 
-    get guerriers() {
-        return this.guerriers_;
-    }
+  set guerriers(a) {
+    this.guerriers_ = a;
+  }
 
-    set guerriers(a) {
-        this.guerriers_ = a;
-    }
+  get marchands() {
+    return this.marchands_;
+  }
 
-    get marchands() {
-        return this.marchands_;
-    }
-
-    set marchands(b) {
-        this.marchands_ = b;
-    }
+  set marchands(b) {
+    this.marchands_ = b;
+  }
 }
 
 module.exports = {Population};
